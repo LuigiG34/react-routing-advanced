936f63175e05bbb3795e29d583d27faea0b8eff2
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-17 15:19:21 +0100

    Output a list of dummy events to the EventsPage Every list item should include a link to the respective EventDetailPage Output the ID of the selected event on the EventDetailPage

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -10,7 +10,7 @@ function MainNavigation() {
             <NavLink to="/" className={({isActive}) => isActive ? classes.active : undefined} end>Home</NavLink>
           </li>
           <li>
-            <NavLink to="/events" className={({isActive}) => isActive ? classes.active : undefined}>Home</NavLink>
+            <NavLink to="/events" className={({isActive}) => isActive ? classes.active : undefined}>Events</NavLink>
           </li>
         </ul>
       </nav>
